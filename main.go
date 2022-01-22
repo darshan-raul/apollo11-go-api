ac1f328249c859d2ad690814d31406875c2a96cd
darshan raul <14124900+darshan-raul@users.noreply.github.com>
2022-01-22 09:27:29 +0530

    refactor: added fiber api code to make graphql call to fastapi

--- main.go
+++ main.go
@@ -5,45 +5,52 @@ import (
   "strings"
   "net/http"
   "io/ioutil"
+  "github.com/gofiber/fiber/v2"
+  "log"
 )
-
-
-
-func make_graphql_call() {
-
-  url := "http://127.0.0.1:8000/graphql"
-  method := "POST"
-
-  payload := strings.NewReader(`{"query":"{student { name city}}"}`)
-
-  client := &http.Client {
-  }
-  req, err := http.NewRequest(method, url, payload)
-
-  if err != nil {
-    fmt.Println(err)
-    return
-  }
-  req.Header.Add("Content-Type", "application/json")
-
-  res, err := client.Do(req)
-  if err != nil {
-    fmt.Println(err)
-    return
-  }
-  defer res.Body.Close()
-
-  body, err := ioutil.ReadAll(res.Body)
-  if err != nil {
-    fmt.Println(err)
-    return
-  }
-  fmt.Println(string(body))
-
-}
 
 
 func main() {
 
-  make_graphql_call()
+  app := fiber.New()
+
+
+  app.Get("/graphql", func(c *fiber.Ctx) error {
+      msg := fmt.Sprintf("✋ %s", c.Params("*"))
+      url := "http://127.0.0.1:8000/graphql"
+      method := "POST"
+
+      payload := strings.NewReader(`{"query":"{student { name city}}"}`)
+
+      client := &http.Client {
+      }
+      req, err := http.NewRequest(method, url, payload)
+
+      if err != nil {
+        fmt.Println(err)
+        return err
+      }
+      req.Header.Add("Content-Type", "application/json")
+
+      res, err := client.Do(req)
+      if err != nil {
+        fmt.Println(err)
+        return err
+      }
+      defer res.Body.Close()
+
+      body, err := ioutil.ReadAll(res.Body)
+      if err != nil {
+        fmt.Println(err)
+        return err
+      }
+      
+      fmt.Println(string(body)+msg)
+      return c.SendString(string(body)) // => ✋ register
+  })
+
+
+  log.Fatal(app.Listen(":3000"))
+
+  // make_graphql_call()
 }
